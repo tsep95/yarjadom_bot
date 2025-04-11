eae6d2edc39e5c984a3166eb10b3c844d2626617
tsep95 <lovest37@gmail.com>
2025-04-11 22:42:51 +0700

    Update yarjadom_bot.py

--- yarjadom_bot.py
+++ yarjadom_bot.py
@@ -260,6 +260,6 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button))
     app.add_handler(CommandHandler("extended", extended))
-    app.add_handler(MessageHandler(Filters.TEXT & ~Filters.COMMAND, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))  # Исправлено Filters -> filters
     logger.info("Бот запущен!")
     app.run_polling()
